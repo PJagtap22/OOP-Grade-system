b5cc878d1ec82dfe8e1c646b3f7486ff8844505f
PJagtap22 <120649300+PJagtap22@users.noreply.github.com>
2023-09-06 19:38:49 +0530

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include "ReportCard.h"
+
+using namespace std;
+
+int main() {
+        string name;
+        int studentID, subject, numSubjects;
+        float gpa;
+
+        cout << "Enter student's name: ";
+        getline(cin, name);
+        cout << "Enter studentID: ";
+        cin >> studentID;
+        cout << "Enter the number of subjects: ";
+        cin >> numSubjects;
+
+        float *subjects = new float[numSubjects];
+        for (int i = 0; i < numSubjects; ++i) {
+            cout << "Enter score of subject" << i+1 << ": ";
+            cin >> subjects[i];
+            }
+
+            Student student(name, studentID);
+            student.CalcGPA(subjects, numSubjects);
+            
+        cin.ignore();
+        student.displayReportCard();
+         
+
+        if (student.isEligible()) {
+            cout << "Pass!" << endl;
+        } else {
+            cout << "Fail!" << endl;
+        }
+ 
+        student.displaySubjectsAndMarks(subjects, numSubjects);
+
+        delete[] subjects;
+        return 0;
+    }
